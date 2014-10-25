86e2876dd080f16c398d00da9c01006225fec343
Maxim Syrykh (@slggr) <slogger1994@gmail.com>
2014-10-25 13:13:29 +0600

    Добавил пару тестов

--- test/checker.test.js
+++ test/checker.test.js
@@ -14,11 +14,31 @@ casper.test.begin('Checker tests', function suite(test) {
       this.sendKeys('#email', '', { reset : true });
 
       // Тестируем с невалидным емейлом
+      this.sendKeys('#email', 'test');
+      actual = casper.evaluate(function() {
+        return document.forms.auth.email.className;
+      });
+      this.test.assertEquals('auth_error', actual, 'Invalid email test [simple text]');
+
+      // Чистим поле
+      this.sendKeys('#email', '', { reset : true });
+
+      // Тестируем с невалидным емейлом
       this.sendKeys('#email', 'test@test');
       actual = casper.evaluate(function() {
         return document.forms.auth.email.className;
       });
-      this.test.assertEquals('auth_error', actual, 'Invalid email test');
+      this.test.assertEquals('auth_error', actual, 'Invalid email test [text@text]');
+
+      // Чистим поле
+      this.sendKeys('#email', '', { reset : true });
+
+      // Тестируем с невалидным емейлом
+      this.sendKeys('#email', 'test@test.a');
+      actual = casper.evaluate(function() {
+        return document.forms.auth.email.className;
+      });
+      this.test.assertEquals('auth_error', actual, 'Invalid email test [short domen zone]');
     })
     .run(function() {
       test.done();

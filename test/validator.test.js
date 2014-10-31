5e1b6e23ef1d12c4718f277002715cb581a95d1b
Michael Uskov <uskovm@gmail.com>
2014-10-31 13:36:59 +0500

    Fixed tests

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,15 +1,17 @@
 var fillEmail = function(browser, email) {
-	var currentInput = browser.getElementAttribute('#email-input', 'value');
+	var currentInput = browser.evaluate(function() {
+		return document.getElementById('email-input').value;
+	});
 	for (var i=0; i<currentInput.length; i++) {
 		browser.sendKeys('#email-input', casper.page.event.key.Backspace);
 	}
 	browser.sendKeys('#email-input', email, {reset: true});
+	console.log(currentInput);
 };
 
 var getValidation = function(browser) {
 	var color = browser.evaluate(function () {
 		return document.getElementById('email-input').parentNode.style.backgroundColor;
-		
 	});
 	return color;
 };
@@ -24,7 +26,7 @@ casper.test.begin('Validation testing', function suite(test) {
 		test.assertEquals(validate(this, 'uskovm@gmail.com'), 'green', 'uskovm@gmail.com');
 		test.assertEquals(validate(this, 'vnbgfjbndkjnv'), 'red', 'vnbgfjbndkjnv');
 		test.assertEquals(validate(this, 'uskovm@gmail.com'), 'green', 'michaeluskov@ya.ru');
-		test.assertEquals(fillEmail(this, ''), undefined, 'Blank input');
+		test.assertEquals(validate(this, ''), '', 'Blank input');
 	}).run(function() {
 		test.done();
 	});

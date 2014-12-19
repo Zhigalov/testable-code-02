c5b2ca9759b9f65faedd3b5df0f635fa91efc747
Puni <Ultimate95@mail.ru>
2014-12-19 23:45:51 +0500

    Fixes tags

--- validator-test.js
+++ validator-test.js
@@ -9,8 +9,9 @@ var checking = function(browser, e_mail){
 }
 
 casper.test.begin('Validation testing', function suite(test) {
-	casper.start('http://localhost:8000/site.html')
+	casper.start('file:///C:/Users/%D0%98%D0%B3%D0%BE%D1%80%D1%8C/Desktop/Institute/%D0%AF%D0%BD%D0%B4%D0%B5%D0%BA%D1%81/testable-code-02/site.html')
 			.then(function() {
+				test.assertEquals(checking(this, ''), 'white');
 				test.assertEquals(checking(this, 'Ultimate95@mail.ru'), 'green');
 				test.assertEquals(checking(this, '123'), 'red');
 				test.assertEquals(checking(this, 'Savichev95@gmail.com'), 'green');
